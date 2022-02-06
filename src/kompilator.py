1fadd06d7dec5021cc80553fb46570f2620c4bd4
agnieszkakurzajewska <agnieszkakurzajewska@hotmail.com>
2022-02-06 23:33:13 +0100

    mess

--- src/kompilator.py
+++ src/kompilator.py
@@ -1,32 +1,13 @@
 import ply.lex as lex
-import ply.yacc as yacc
 from grammar import *
-from tokens import *
 from lexer import *
+from tokens import *
+import sys
 
-# print("\n\n")
-# while True: 
-#     tok = lexer.token() 
-#     if not tok: 
-#         break # Nigdy więcej input 
-#     print("Typ: ", end = '')
-#     print(tok.type.ljust(15), end = '')
-#     print("     | wartosc: ", end = '')
-#     print(str(tok.value).ljust(10), end = '')
-#     print("     | numer linii: : ", end = '')
-#     print(str(tok.lineno).ljust(10), end = '')
-#     print("     | lexpos: ", end = '')
-#     print(tok.lexpos)
+input = open(sys.argv[1], "r");
+input_text = input.read()
 
-
-data = '''
-VAR n BEGIN
-n ASSIGN 3;
-WRITE n;
-END
-'''
 lexer = lex.lex()
-lexer.input(data)
 parser = yacc.yacc()
-parser.parse(data)
- 
+
+parser.parse(input_text, lexer=lexer)
--- src/lexer.py
+++ src/lexer.py
@@ -3,16 +3,10 @@ def t_num(t):
     t.value = int(t.value)    
     return t
 
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
-
-# Error handling rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
+
+t_ignore  = ' \t\n'
 
 t_pidentifier = r'[_a-z]+'
